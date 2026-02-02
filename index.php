3767bdfe969470095477d06021615e1f30708235
Dinis Neves <dinisneves100@gmail.com>
2026-02-02 23:15:16 +0000

    create slideshow for home page

--- index.php
+++ index.php
@@ -11,5 +11,21 @@
         <img src="img/logods.png" />
       </a>
     </header>
+
+    <!-- Carousel Imgs -->
+    <div class="slides">
+      <div class="slide slide-1">
+        <img src="img/est1.jpg" alt="Imagem do estádio 1" />
+      </div>
+      <div class="slide slide-2">
+        <img src="img/est2.jpg" alt="Imagem do estádio 2" />
+      </div>
+      <div class="slide slide-3">
+        <img src="img/casaco-es1.png" alt="Imagem do casaco 1" />
+      </div>
+      <div class="slide slide-4">
+        <img src="img/casaco-es2.png" alt="Imagem do casaco 2" />
+      </div>
+    </div>
   </body>
 </html>
