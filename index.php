de592abf364ffe14628f90a644c25b6b3daca152
Dinis Neves <dinisneves100@gmail.com>
2026-02-02 21:14:11 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>United</title>
+    <link rel="icon" type="image/x-icon" href="logo.jpg">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+      <a href="index.php" class="logo">
+        <img src="img/logods.png" />
+      </a>
+    </header>
+  </body>
+</html>
