ad7c1816cc54693560e2371db2d3af935b1492e5
DINISBN <dinisneves100@gmail.com>
2026-02-03 22:20:12 +0000

    add initial catagolo page

--- catalogo.php
+++ catalogo.php
@@ -4,6 +4,7 @@
     <title>Catálogo</title>
     <link rel="icon" type="image/x-icon" href="logo.jpg">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/catalogo.css">
   </head>
   <body>
     <header>
@@ -11,6 +12,31 @@
         <img src="img/logods.png" />
       </a>
     </header>
-    
+
+    <div class="container-fluid featured-products common-products-section">
+      <div class="container">
+        <div class="row">
+          <!-- Titulo Page -->
+          <div class="row-1">
+            <h2 class="block-header">Produtos em destaque</h2>
+            <span class="block-header-separator"></span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="row-2">
+            <div class="box-product-block">
+              <div class="product-block">
+                <div class="product-block-box">
+                  <div class="product-block-box_content">
+                    <!-- Desconto Badge -->
+                    <div class=""></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
