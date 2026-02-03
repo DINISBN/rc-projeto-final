ccfff4ed75d476ed3df0d3de891b85ab6395fbcf
Dinis Neves <dinisneves100@gmail.com>
2026-02-03 20:43:04 +0000

    add catalog page & footer

--- index.php
+++ index.php
@@ -10,22 +10,40 @@
       <a href="index.php" class="logo">
         <img src="img/logods.png" />
       </a>
+      <a href="catalogo.php" class="page">
+        <h3>Catálogo</h3> 
+      </a>
     </header>
 
     <!-- Carousel Imgs -->
     <div class="slides">
       <div class="slide slide-1">
-        <img src="img/est1.jpg" alt="Imagem do estádio 1" />
+        <a href="">
+          <img src="img/est1.jpg" alt="Imagem do estádio 1" />
+        </a>
       </div>
       <div class="slide slide-2">
-        <img src="img/est2.jpg" alt="Imagem do estádio 2" />
+        <a href="">
+          <img src="img/est2.jpg" alt="Imagem do estádio 2" />
+        </a>
       </div>
       <div class="slide slide-3">
-        <img src="img/casaco-es1.png" alt="Imagem do casaco 1" />
+        <a href="">
+          <img src="img/casaco-es1.png" alt="Imagem do casaco 1" />
+        </a>
       </div>
       <div class="slide slide-4">
-        <img src="img/casaco-es2.png" alt="Imagem do casaco 2" />
+        <a href="">
+          <img src="img/casaco-es2.png" alt="Imagem do casaco 2" />
+        </a>
       </div>
     </div>
+    <section>    
+      <p>Man United x <span>United</span></p>
+    </section>
+    <footer>
+      <p class="paragrafo">&copy; 2026 United</p>
+      <p class="paragrafo">Todos os Direitos Reservados</p>
+    </footer>
   </body>
 </html>
